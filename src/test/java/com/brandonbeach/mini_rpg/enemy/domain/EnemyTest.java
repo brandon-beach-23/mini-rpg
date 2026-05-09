bc5afd01bd78e1e6ec0b0cfac73541ca44b52564
Brandon Beach <brandon.beach23@gmail.com>
2026-05-09 17:49:46 -0400

    Added skeleton for EnemyTest

---
+++ src/test/java/com/brandonbeach/mini_rpg/enemy/domain/EnemyTest.java
@@ -0,0 +1,46 @@
+package com.brandonbeach.mini_rpg.enemy.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class EnemyTest {
+
+    @Test
+    public void testEnemyCreation_ValidInputs(){
+
+    }
+
+    @Test
+    public void testEnemyCreation_InvalidNameTooLong(){
+
+    }
+
+    @Test
+    public void testEnemyCreation_InvalidNameNonAlpha(){
+
+    }
+
+    @Test
+    public void testTakeDamage_ReduceHitPoints() {
+
+    }
+
+    @Test
+    public void testTakeDamage_SetStatusToDeadWhenHitPointsReachZero() {
+
+    }
+
+    @Test
+    public void testApplyStatusEffect_ChangesEnemyStatus(){
+
+    }
+
+    @Test
+    public void testCureCondition_ResetsStatusToHealthy(){
+
+    }
+
+    @Test
+    public void testIsAlive_ReturnsTrueWhenHealthy(){
+
+    }
+}
